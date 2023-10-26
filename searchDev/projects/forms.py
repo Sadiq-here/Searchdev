f49bf3e455c248c93c61e44d259eca48806a2b4e
Sadiq <mdsadique2912@gmail.com>
2023-10-25 17:51:00 -0700

    CRUD operations created

---
+++ searchDev/projects/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import Project
+
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title','description','source_link', 'demo_link', 'tags']
